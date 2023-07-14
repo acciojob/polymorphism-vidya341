c49f6fc5967710a206186df09eeee711bcb32fcd
vidya <vidyashivakumar13@gmail.com>
2023-07-14 19:34:46 +0530

    changes done assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,13 +3,36 @@ package com.driver;
 import com.sun.xml.internal.bind.v2.runtime.output.SAXOutput;
 import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
+
 public class Main {
+    public static class Product {
+        //constructor
+        //final commit
+        public int product(int x,int y)
+        {
+            return x*y;
+        }
+        public int product(int x,int y,int z)
+        {
+            return x*y*z;
+        }
+        public double product(double x,double y)
+        {
+            return x*y;
+        }
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+        int a = p.product(5,10);
+        int b = p.product(5,10,5);
+        double c = p.product(5.2,10.2);
+
+    }
     //product class
     //final commit
-    Product p = new Product();
-    int a = p.product(5,10);
-    int b = p.product(5,10,5);
-    double c = p.product(5.2,10.2);
+
+
 
 
 }
